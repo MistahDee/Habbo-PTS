d125b637b53eb44e11d0abeec63017f0f83ed568
MistahDee <33980441+MistahDee@users.noreply.github.com>
2017-11-26 13:26:28 +0000

    w.e

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express'),
     app     = express(),
     core    = require('./core/core.js');
 
-    core = new core(app, express, function(d) {
-      //Start listening here.
-    });
+core = new core(app, express, function(d) {
+//Start listening here.
+});
